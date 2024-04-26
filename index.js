9c1a5d36b547ab3da4003e0a410cdf7824517ab3
22machinist <sharmashutosh22@gmail.com>
2024-04-27 02:36:29 +0530

    added the code

--- index.js
+++ index.js
@@ -68,3 +68,5 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
